8e221ecc5a9e606587b8243c658f29710481d1c3
Bhavya Likhitha Bukka <94377624+BhavyaLikhitha@users.noreply.github.com>
2026-07-10 17:25:11 -0400

    Time: 3287 ms (84.54%), Space: 0B (100%) - LeetHub

---
+++ 1907-count-salary-categories/1907-count-salary-categories.sql
@@ -0,0 +1,8 @@
+SELECT 'Low Salary'     AS category, 
+COUNT(*) AS accounts_count FROM accounts WHERE income < 20000
+UNION ALL
+SELECT 'Average Salary' AS category, 
+COUNT(*) AS accounts_count FROM accounts WHERE income BETWEEN 20000 AND 50000
+UNION ALL
+SELECT 'High Salary'    AS category, 
+COUNT(*) AS accounts_count FROM accounts WHERE income > 50000;
